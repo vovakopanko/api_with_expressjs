85574a9e9f825bfdefe18f1300a9f35fa912e124
vovakopanko <vovakopanko@mail.ru>
2023-02-17 15:25:32 +0100

    routing

--- index.js
+++ index.js
@@ -3,9 +3,42 @@ import express from "express";
 const port = 8000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.all("/hello", (req, res, next) => {
+  console.log("Go to the next step => ...");
+  next();
 });
+
+const cb = (req, res, next) => {
+  console.log(" step 2 ");
+  next();
+};
+
+const cb2 = (req, res, next) => {
+  console.log(" step 3 ");
+  next();
+};
+
+// app.get("/hello", cb, (req, res) => {
+//   console.log('GET')
+//   res.send("Hello 1");
+// });
+
+// app.route('/user).get("/hello", cb, (req, res) => {
+//   console.log('GET')
+//   res.send("Hello 1")
+//      .post("/hello", (req, res) => {
+//   res.send(POST")
+// })
+// });
+
+app.get("/hello", [
+  cb,
+  cb2,
+  (req, res) => {
+    console.log("GET");
+    res.send("Hello 1");
+  },
+]);
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
