fad33a350b7da0c0ce068a4aff1b3294574dc537
vovakopanko <vovakopanko@mail.ru>
2023-02-17 14:46:54 +0100

    add express

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-import http from "http";
+import express from "express";
 
-const host = "127.0.0.1";
 const port = 8000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello");
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
 
-server.listen(port, host, () => {
-  console.log(`Server started on ${port}:${host}`);
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
 });
